223c4500602d2df9f0abe28abb9f4bb403e1ee1d
winter <winter@gmai.com>
2024-04-21 09:58:53 +0700

    Add project files.

---
+++ MiniMartSystem/Function.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniMartSystem
+{
+    public class Function
+    {
+
+
+
+    
+        public static void MiniMartApplication()
+        {
+            bool exit = false;
+            do
+            {
+                Console.WriteLine("1. Mini-Mart");
+                Console.WriteLine("2. Add Item");
+                Console.WriteLine("3. Exit");
+                Console.Write("Please enter your transaction: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        MiniMart();
+                        break;
+                    case "2":
+                       AddItem();
+                        break;
+                    case "3":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            } while (!exit);
+        }
+
+        public static void MiniMart()
+        {
+            Console.WriteLine("Mini-Mart Application");
+
+            for (int i = 0; i < MiniMartSystem.items.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {MiniMartSystem.items[i]}: {MiniMartSystem.prices[i]}R");
+            }
+
+            Console.Write("Which item do you want to buy?: ");
+            int choice = int.Parse(Console.ReadLine()) - 1;
+
+            Console.WriteLine($"{MiniMartSystem.items[choice]}: {MiniMartSystem.prices[choice]}R");
+            Console.Write("QTY: ");
+            int qty = int.Parse(Console.ReadLine());
+            int total = CalculateTotal(MiniMartSystem.prices[choice], qty);
+            Console.WriteLine($"Total: {total}R");
+
+            Console.Write("Get money: ");
+            double money = double.Parse(Console.ReadLine());
+
+            double paybackRiel = CalculatePaybackRiel(money, total);
+            double paybackDollar = CalculatePaybackDollar(paybackRiel);
+
+            Console.WriteLine($"Payback:");
+            Console.WriteLine($"1. Riel: {paybackRiel}R");
+            Console.WriteLine($"2. Dollar: {paybackDollar}$");
+
+            Console.Write("Do you want to continue? (Yes/No): ");
+            string cont = Console.ReadLine();
+
+            if (cont.ToLower() == "yes")
+                MiniMartApplication();
+            else
+                Console.WriteLine("Welcome to Mini-mart System");
+        }
+        public static int CalculateTotal(int price, int qty)
+        {
+            return price * qty;
+        }
+
+        public static double CalculatePaybackRiel(double money, int total)
+        {
+            return money - total;
+        }
+
+        public static double CalculatePaybackDollar(double paybackRiel)
+        {
+            return paybackRiel / MiniMartSystem.exchangeRate;
+        }
+
+
+
+        public static void AddItem()
+        {
+            Console.WriteLine("Add Item");
+            Console.Write("Enter number of Item for add: ");
+            int numItems = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numItems; i++)
+            {
+                Console.Write("Enter Item-Name: ");
+                string itemName = Console.ReadLine();
+                Console.Write("Enter Price: ");
+                int itemPrice = int.Parse(Console.ReadLine());
+                Console.Write("Enter QTY: ");
+                int itemQty = int.Parse(Console.ReadLine());
+
+                Array.Resize(ref MiniMartSystem.items, MiniMartSystem.items.Length + 1);
+                Array.Resize(ref MiniMartSystem.prices, MiniMartSystem.prices.Length + 1);
+                Array.Resize(ref MiniMartSystem.quantities, MiniMartSystem.quantities.Length + 1);
+
+                MiniMartSystem.items[MiniMartSystem.items.Length - 1] = itemName;
+                MiniMartSystem.prices[MiniMartSystem.prices.Length - 1] = itemPrice;
+                MiniMartSystem.quantities[MiniMartSystem.quantities.Length - 1] = itemQty;
+            }
+
+            Console.Write("Do you want to add more? (Yes/No): ");
+            string cont = Console.ReadLine();
+
+            if (cont.ToLower() == "yes")
+                AddItem();
+            else
+                MiniMartApplication();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+}
+
---
+++ MiniMartSystem/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MiniMartSystem
+{
+    class MiniMartSystem
+    {
+        public static string username = "";
+        public static string password = "";
+        public static string[] items = { "Coca", "Fanta", "ABC", "Cambodia", "Ice-cream", "Viso" };
+        public static int[] prices = { 2000, 2000, 2000, 5000, 3000, 4000 };
+        public static int[] quantities = { 10, 10, 10, 10, 10, 10 };
+        public static double exchangeRate = 4000;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mini-mart System");
+
+            bool loggedIn = false;
+            do
+            {
+                Console.Write("Username: ");
+                username = Console.ReadLine();
+                Console.Write("Password: ");
+                password = Console.ReadLine();
+                username = username.ToLower();
+                password = password.ToLower();
+
+                if (username.Equals("admin") && password.Equals("admin"))
+                {
+                    loggedIn = true;
+                    Console.WriteLine("Welcome, admin!");
+                    Function.MiniMartApplication();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username/password");
+                }
+            } while (!loggedIn);
+        }
+    }
+}
